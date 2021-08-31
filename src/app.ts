bb8d8736b04c2b2ed6618e1401ce9bd73bac36e4
HyunSJ <dudtod1596@gmail.com>
2021-08-31 22:13:54 +0900

    update ClassDiagram, add note video todo components

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
-import { ImageComponent } from "./components/page/item/iamge.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 import { PageComponet } from "./components/page/page.js";
 
 class App {
@@ -7,11 +10,24 @@ class App {
     this.page = new PageComponet();
     this.page.attachTo(appRoot);
     //
+
     const image = new ImageComponent(
       "Image Title",
       "https://picsum.photos/600/300"
     );
     image.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://www.youtube.com/embed/7LNl2JlZKHA"
+    );
+    video.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    note.attachTo(appRoot, "beforeend");
+
+    const todo = new TodoComponent("Todo Title", "Todo Item");
+    todo.attachTo(appRoot, "beforeend");
   }
 }
 
